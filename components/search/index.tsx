2cc73c09943f302c2226a157a4a0f14d6fa254e9
Michael Zhu <mzhu23@gmail.com>
2025-05-22 16:11:14 -0700

    Styled Search component

--- components/search/index.tsx
+++ components/search/index.tsx
@@ -2,6 +2,9 @@
  * Copyright (c) HashiCorp, Inc.
  * SPDX-License-Identifier: MPL-2.0
  */
+import s from './style.module.css'
+import Image from 'next/image'
+import searchIcon from './img/search-icon.svg'
 
 export interface SearchProps {
 	onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void
@@ -14,15 +17,31 @@ export default function Search({
 }: SearchProps) {
 	return (
 		<>
-			<input
-				type="text"
-				placeholder="Search people by name"
-				onChange={onInputChange}
-			/>
+			<div className={s.searchContainer}>
+				<Image
+					priority
+					width={20}
+					height={20}
+					src={searchIcon}
+					alt={'Search icon'}
+				/>
+				<input
+					className={s.searchInput}
+					type="text"
+					placeholder="Search people by name"
+					onChange={onInputChange}
+				/>
+			</div>
 
-			<div>
-				<input type="checkbox" onChange={onProfileChange} />
-				<div>Hide people missing a profile image</div>
+			<div className={s.hideProfileContainer}>
+				<input
+					type="checkbox"
+					className={s.hideProfileCheckbox}
+					onChange={onProfileChange}
+				/>
+				<div className={s.hideProfileLabel}>
+					Hide people missing a profile image
+				</div>
 			</div>
 		</>
 	)
